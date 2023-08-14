98dae7ee9cab02f4abb8a7e7daec4d5b69629528
abdelbasset-mg <a_meghraoui@estin.dz>
2023-08-14 13:54:12 +0100

    final page1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 
@@ -18,23 +20,30 @@ void main() {
               mainAxisSize: MainAxisSize.min,
               children: [
                 Image.asset('quiz-logo.png', width: 200),
-                const Text('Learn flutter in the fun way!', style: TextStyle(fontSize: 28, color: Colors.white),),
+                const Padding(padding: EdgeInsets.all(30.0)),
+                const Text('Learn flutter in the fun way !', style: TextStyle(fontSize: 20, color: Colors.white),),
                 TextButton(
                   onPressed: () {}, 
-                 child: const Text('Start Quiz', style: TextStyle(fontSize: 24, color: Colors.white),),
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(40),
+                  ),
+                  child: const Text('START QUIZ', style: TextStyle(fontSize: 28, color: Colors.white),),
+                  ),
+                  ],
+                  ),
                  ),
 
 
-            ],
+                
           )
           ) 
             
           ),
-        ),
-      ),
+        );
+      
 
 
-    );
+    
   
   
 }
