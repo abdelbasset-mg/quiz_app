0aa2b9664d46ecd5d7142226713aefa38d0ff4c3
abdelbasset-mg <a_meghraoui@estin.dz>
2023-08-15 15:49:32 +0100

    dynamic answers manipulation and screen switching

--- lib/models/quiz_questions.dart
+++ lib/models/quiz_questions.dart
@@ -2,4 +2,11 @@ class QuizQuestions {
   const QuizQuestions(this.text, this.answers);
   final String text; 
   final List<String> answers;
+
+  List<String> shuffeledList () {
+    final List<String> shuffeledAnswers = List.of(answers);
+    shuffeledAnswers.shuffle();
+    return shuffeledAnswers;
+  }
+
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -10,24 +10,40 @@ class QuestoinsScreen extends StatefulWidget {
   }
 }
 
+
+
 class _QuestoinsScreenState extends State<QuestoinsScreen> {
+  var currentQuestionIndex = 0;
+
+void nextqestion(){
+  setState((){
+    currentQuestionIndex = currentQuestionIndex + 1;
+  });
+}
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
     return SizedBox(
         width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-             Text(
-                currentQuestion.text,
-                style: const TextStyle(fontSize: 16, color: Colors.white)),
-            const SizedBox(height: 30),
-            AnswerButton(toTap: () {}, answerText: currentQuestion.answers[0]),
-            AnswerButton(toTap: () {}, answerText: currentQuestion.answers[1]),
-            AnswerButton(toTap: () {}, answerText: currentQuestion.answers[2]),
-            AnswerButton(toTap: () {}, answerText: currentQuestion.answers[3]),
-          ],
+        child: Container(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+               Text(
+                  currentQuestion.text,
+                  style: const TextStyle(fontSize: 16, color: Colors.white),
+                  textAlign: TextAlign.center,
+
+                  ),
+              const SizedBox(height: 30),
+              ...currentQuestion.shuffeledList().map((answer){
+                return AnswerButton(answerText: answer, toTap:nextqestion);
+              }),
+              
+            ],
+          ),
         )
         );
   }
